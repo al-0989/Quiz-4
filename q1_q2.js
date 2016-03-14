6bccd6f4cfe3795d23d5ba260c1effe0e7bd9cc9
An La <a.ngoc.la@gmail.com>
2016-03-14 09:42:36 -0700

    Solution for question 1 + 2

--- q1_q2.js
+++ q1_q2.js
@@ -1,1 +1,8 @@
-$("span.txt:contains('eal')").css("color","red") // Solution to question 1
+// Solution to question 1
+$("span.txt:contains('eal')").css("color","red");
+
+// Solution to Question 2
+$("a").on("click", function(event){
+  event.preventDefault();
+  $(this).fadeOut();
+});
